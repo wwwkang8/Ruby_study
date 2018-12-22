9f8471736349cda42b2b0ba7a1c6c761cca5adba
JeongHo <wwwkang8@gmail.com>
2018-12-22 23:36:29 +0900

    Ruby style guide

---
+++ ruby_style_guide.rb
@@ -0,0 +1,25 @@
+# 루비 스타일 가이드 깃허브
+# https://github.com/rubocop-hq/ruby-style-guide
+
+#snake 케이스식의 표현식
+# 메서드, 변수 이름은 Snake Case로 기입
+this_is_snake_case
+
+also_snake_case
+
+# 루비에서는 Tab이 다른 언어의 Tab보다 depth가 적다.
+# 루비에서는 Tab을 2번 친다.
+def my_name_is
+  # and my code is here
+end
+
+#클래스이름은 CamelCase로 한다.
+CamelCase
+
+ApplicationController
+
+
+# 클래스 이름은 CamelCase로 기입
+class SomeClass
+
+end
